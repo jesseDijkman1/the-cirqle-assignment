673f3ff721f6cd9117d8f9a177d74c095825949e
Jesse Dijkman <jesse.dijkman@live.nl>
2024-12-20 11:40:47 +0100

    chore: Add basic Heading & Image component

---
+++ components/global/Image.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-32 h-32 bg-gray-500 rounded-full overflow-hidden">
+    <img
+      v-if="src"
+      :src="src"
+      :alt="alt"
+      class="w-full h-full object-cover object-center"
+    />
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ["src", "alt"],
+  };
+</script>
