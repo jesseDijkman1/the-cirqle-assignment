ceff725c5a03028a9a9ae54d8dc18e53708f0f25
Jesse Dijkman <jesse.dijkman@live.nl>
2024-12-22 21:47:19 +0100

    feat: Add paragraph component

---
+++ components/ui/Paragraph.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-if="isLoading" class="flex flex-col space-y-2">
+    <div
+      v-for="index in loaderLines"
+      class="h-4 bg-skeleton-loader rounded"
+      :style="{
+        width: `${((loaderLines - (index - 1)) / loaderLines) * 100}%`,
+      }"
+    ></div>
+  </div>
+  <p v-else><slot /></p>
+</template>
+
+<script>
+  export default {
+    props: {
+      isLoading: {
+        type: Boolean,
+        default: false,
+      },
+      loaderLines: {
+        type: Number,
+        default: 1,
+      },
+    },
+  };
+</script>
