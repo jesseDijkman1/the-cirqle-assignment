7b27f2db094d350dbc85b3abf54d130d45e9d05f
Jesse Dijkman <jesse.dijkman@live.nl>
2024-12-22 11:40:40 +0100

    feat: Add line graph to metric component

--- components/ui/Metric.vue
+++ components/ui/Metric.vue
@@ -1,29 +1,35 @@
 <template>
   <div
-    class="w-full aspect-square relative text-white bg-[rgba(0,0,0,.4)] flex flex-col justify-center items-center rounded-lg"
+    class="w-full aspect-square relative text-white bg-[rgba(0,0,0,.4)] flex flex-col justify-center items-center rounded-lg z-0 pb-4"
   >
     <p class="absolute top-1 left-2 text-green-400">{{ label }}</p>
 
-    <button class="absolute top-0 right-0 p-2 w-auto text-light-grey">
-      <IconStar />
-    </button>
+    <div class="relative w-full flex justify-center">
+      <strong :style="`color: ${color}`" class="text-4xl">
+        {{ totalFixed }}
+      </strong>
 
-    <strong :style="`color: ${color}`" class="text-4xl">
-      {{ totalFixed }}
-    </strong>
+      <div
+        class="pt-1 absolute left-0 top-full w-full flex justify-center space-x-4 text-xs"
+      >
+        <p class="text-light-grey flex space-x-1 items-center">
+          <IconOrganic />
+          <span>{{ organicFixed }}</span>
+        </p>
 
-    <div
-      class="px-2 pb-4 absolute left-0 bottom-0 w-full flex flex-col items-center"
-    >
-      <p class="text-light-grey flex space-x-2 items-center">
-        <IconOrganic />
-        <span>{{ organicFixed }}</span>
-      </p>
+        <p class="text-light-grey flex space-x-1 items-center">
+          <IconPaid />
+          <span>{{ paidFixed }}</span>
+        </p>
+      </div>
+    </div>
 
-      <p class="text-light-grey flex space-x-2 items-center">
-        <IconPaid />
-        <span>{{ paidFixed }}</span>
-      </p>
+    <div class="absolute bottom-0 left-0 w-full h-1/3 z-[-1] p-4 opacity-50">
+      <UILineGraph
+        v-if="graphData"
+        :data="graphData"
+        :valueKey="graphValueKey"
+      />
     </div>
   </div>
 </template>
@@ -33,7 +39,15 @@
 
   // Maybe add some props that define a threshold so the metric can quickly indicate whether it's positive or not
   export default {
-    props: ["label", "organic", "paid", "total", "thresholds"],
+    props: [
+      "label",
+      "organic",
+      "paid",
+      "total",
+      "thresholds",
+      "graphData",
+      "graphValueKey",
+    ],
     computed: {
       organicFixed() {
         return (typeof this.organic === "number" ? this.organic : 0).toFixed(2);
