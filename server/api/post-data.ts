708717904def3b6d0f596c84d507e92edf6fa987
Jesse Dijkman <jesse.dijkman@live.nl>
2024-12-22 19:26:37 +0100

    feat: Create api route for fetching the data

---
+++ server/api/post-data.ts
@@ -0,0 +1,28 @@
+import * as path from "node:path";
+import * as fs from "node:fs";
+
+export default defineEventHandler(async (event) => {
+  const summaryFilePath = path.join(process.cwd(), "public", "summary.json");
+  const creatorFilePath = path.join(process.cwd(), "public", "creator.json");
+  const trendingFilePath = path.join(process.cwd(), "public", "trending.json");
+
+  try {
+    const [summaryData, creatorData, trendingData] = await Promise.all([
+      fs.promises.readFile(summaryFilePath, "utf-8"),
+      fs.promises.readFile(creatorFilePath, "utf-8"),
+      fs.promises.readFile(trendingFilePath, "utf-8"),
+    ]);
+
+    return {
+      summary: JSON.parse(summaryData),
+      creator: { ...JSON.parse(creatorData).data.creator },
+      trending: JSON.parse(trendingData),
+    };
+  } catch (err) {
+    return {
+      summary: null,
+      creator: null,
+      trending: null,
+    };
+  }
+});
