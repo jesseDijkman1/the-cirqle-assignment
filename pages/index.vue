c53049ef2ddf388230180a6511166f2ae157be09
Jesse Dijkman <jesse.dijkman@live.nl>
2024-12-22 21:12:59 +0100

    feat: Add slight delay with data fetch to show skeleton loader

--- pages/index.vue
+++ pages/index.vue
@@ -23,8 +23,12 @@
       this.fetchData();
     },
     methods: {
+      async wait(delay) {
+        return new Promise((resolve) => setTimeout(resolve, delay));
+      },
       async fetchData() {
         try {
+          await this.wait(200);
           const response = await fetch("/api/post-data");
           this.data = await response.json();
         } catch (error) {
